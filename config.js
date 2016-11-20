596c067d071174a637d6d581d42ccd7d7081cd10
Sven Schannak <EPATROL-Sven@Svens-iMac.fritz.box>
2016-11-20 16:39:51 +0100

    change bug in config file

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ import React from 'react';
 import Routes from './routes';
 import Reducer from './reducers/reducer';
 
-export const Routes = UserManagementRoutes;
+export const Routes = Routes;
 export const Reducer = [Reducer];
